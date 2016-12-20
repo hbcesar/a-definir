9654662b101d87a585b3e8c1549f457ea7828485
EpicIcaro <icaro@easy.com>
2016-12-20 09:11:51 -0200

    DesabafoDAO

---
+++ ADefinir/DesabafoDAO.swift
@@ -0,0 +1,26 @@
+//
+//  DesabafoDAO.swift
+//  ADefinir
+//
+//  Created by Student on 12/20/16.
+//  Copyright © 2016 LuizMai & CesarBernabe. All rights reserved.
+//
+
+import Foundation
+import CoreData
+import UIKit
+
+class DesabafoDAO {
+    static func inserir(desabafo: Desabafo) {
+        //inserir um desabafo no BD
+        
+    }
+    
+    static func comentar(desabafo: Desabafo) {
+        //inserir comentario para um certo desabafo
+    }
+    
+    static func buscarTodos() -> [Desabafo] {
+        //retornar lista de todos os desabafos
+    }
+}
